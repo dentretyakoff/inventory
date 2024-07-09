c9a7bbb41e068889a1fbec59422a53f9b638ad21
Denis Tretyakov <den.tretyakoff@yandex.ru>
2024-07-09 16:50:13 +0700

    Попробую добавить сводные данные по пользователям.

--- invent_pc/users/models.py
+++ invent_pc/users/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class StatusChoices(models.TextChoices):
+    ACTIVE = 'active', 'Активен'
+    INACTIVE = 'inactive', 'Неактивен'
+
+
+class ADusers(models.Model):
+    fio = models.TextField()
+    adlogin = models.TextField()
+    admail = models.TextField(blank=True)
+    adgroup = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=15,
+        choices=StatusChoices.choices,
+        default=StatusChoices.INACTIVE,
+        verbose_name='Статус в AD',
+    )
+    rdlogin = models.ForeignKey(
+        'Radius',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    vpn = models.ForeignKey(
+        'VPN',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f'{self.fio} ({self.adlogin})'
+
+
+class Radius(models.Model):
+    fio = models.TextField()
+    rdlogin = models.TextField()
+    status = models.CharField(
+        max_length=15,
+        choices=StatusChoices.choices,
+        default=StatusChoices.INACTIVE,
+        verbose_name='Статус в Radius',
+    )
+
+    def __str__(self):
+        return f'{self.rdlogin}'
+
+
+class VPN(models.Model):
+    name = models.TextField()
+    comment = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=15,
+        choices=StatusChoices.choices,
+        default=StatusChoices.INACTIVE,
+        verbose_name='Статус VPN',
+    )
+
+    def __str__(self):
+        return f'{self.name}'
