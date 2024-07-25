9d6c265e259d55cea5d2c1a7e6e69580244486f1
Denis Tretyakov <den.tretyakoff@yandex.ru>
2024-07-25 18:37:35 +0700

    Добавил админку.

--- invent_pc/comps/models.py
+++ invent_pc/comps/models.py
@@ -14,24 +14,31 @@ class ItemsChoices(models.TextChoices):
 class Disk(models.Model):
     model = models.CharField('Модель', max_length=100)
     install_date = models.DateTimeField(
-        'Дата установки', auto_now_add=True
+        'Дата установки',
+        auto_now_add=True
     )
-    uninstall_date = models.DateTimeField('Дата снятия',
-                                          null=True,
-                                          blank=True,
-                                          default=None)
+    uninstall_date = models.DateTimeField(
+        'Дата снятия',
+        null=True,
+        blank=True,
+        default=None
+    )
     capacity = models.CharField('Объем, Гб.', max_length=20)
     serial_number = models.CharField(
         'Серийный номер',
         max_length=100,
         null=True,
         blank=True,
-        default='Отсутствует')
-    comp = models.ForeignKey('Comp',
-                             related_name='disks',
-                             on_delete=models.CASCADE,
-                             null=True,
-                             blank=True)
+        default='Отсутствует'
+    )
+    comp = models.ForeignKey(
+        'Comp',
+        related_name='disks',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name='Компьютер'
+    )
     status = models.CharField(
         max_length=15,
         choices=ItemsChoices.choices,
@@ -43,32 +50,38 @@ class Disk(models.Model):
         return f'{self.capacity} GB'
 
     class Meta:
-        ordering = ['uninstall_date']
+        ordering = ('uninstall_date',)
+        verbose_name = 'Диск'
+        verbose_name_plural = 'Диски'
 
 
 class Ram(models.Model):
     model = models.CharField('Модель', max_length=100)
     install_date = models.DateTimeField(
-        'Дата установки', auto_now_add=True
+        'Дата установки',
+        auto_now_add=True
     )
     uninstall_date = models.DateTimeField(
         'Дата снятия',
         null=True,
         blank=True,
-        default=None)
+        default=None
+    )
     capacity = models.CharField('Объем, Гб.', max_length=20)
     serial_number = models.CharField(
         'Серийный номер',
         max_length=100,
         null=True,
         blank=True,
-        default='Отсутствует')
+        default='Отсутствует'
+    )
     comp = models.ForeignKey(
         'Comp',
         related_name='rams',
         on_delete=models.CASCADE,
         null=True,
-        blank=True)
+        blank=True
+    )
     status = models.CharField(
         max_length=15,
         choices=ItemsChoices.choices,
@@ -80,32 +93,39 @@ class Ram(models.Model):
         return f'{self.capacity} GB'
 
     class Meta:
-        ordering = ['uninstall_date']
+        ordering = ('uninstall_date',)
+        verbose_name = 'RAM'
+        verbose_name_plural = 'RAMs'
 
 
 class Monitor(models.Model):
     model = models.CharField('Модель', max_length=100)
     install_date = models.DateTimeField(
-        'Дата установки', auto_now_add=True
+        'Дата установки',
+        auto_now_add=True
     )
     uninstall_date = models.DateTimeField(
         'Дата снятия',
         null=True,
         blank=True,
-        default=None)
+        default=None
+    )
     manufacturer = models.CharField('Производитель', max_length=50)
     serial_number = models.CharField(
         'Серийный номер',
         max_length=100,
         null=True,
         blank=True,
-        default='Отсутствует')
+        default='Отсутствует'
+    )
     comp = models.ForeignKey(
         'Comp',
         related_name='monitors',
         on_delete=models.CASCADE,
         null=True,
-        blank=True)
+        blank=True,
+        verbose_name='Компьютер'
+    )
     status = models.CharField(
         max_length=15,
         choices=ItemsChoices.choices,
@@ -117,7 +137,9 @@ class Monitor(models.Model):
         return f'{self.manufacturer} {self.model}'
 
     class Meta:
-        ordering = ['uninstall_date']
+        ordering = ('uninstall_date',)
+        verbose_name = 'Монитор'
+        verbose_name_plural = 'Мониторы'
 
 
 class WebCamera(models.Model):
@@ -125,6 +147,11 @@ class WebCamera(models.Model):
 
     def __str__(self):
         return self.model
+
+    class Meta:
+        ordering = ('model',)
+        verbose_name = 'Веб-камера'
+        verbose_name_plural = 'Веб-камеры'
 
 
 class Department(models.Model):
@@ -134,11 +161,17 @@ class Department(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Департамент'
+        verbose_name_plural = 'Департаменты'
+
 
 class Comp(models.Model):
     pc_name = models.CharField('Имя ПК', max_length=100)
     add_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True
+        'Дата добавления',
+        auto_now_add=True
     )
     online_date = models.DateTimeField(
         'Дата последнего подключения',
@@ -148,12 +181,22 @@ class Comp(models.Model):
     os_arch = models.CharField('Архитектура', max_length=20)
     cpu = models.CharField('Процессор', max_length=50)
     motherboard = models.CharField('Материнская плата', max_length=100)
-    department = models.ForeignKey(Department,
-                                   related_name='comp',
-                                   on_delete=models.CASCADE)
-    web_camera = models.ForeignKey(WebCamera,
-                                   related_name='comp',
-                                   on_delete=models.CASCADE)
+    department = models.ForeignKey(
+        Department,
+        related_name='comp',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Департамент'
+    )
+    web_camera = models.ForeignKey(
+        WebCamera,
+        related_name='comp',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Веб-камера'
+    )
 
     def is_offline_long_time(self):
         """Определение компьютеров с большим офлайн периодом, для удаления."""
@@ -164,7 +207,9 @@ class Comp(models.Model):
         return self.pc_name
 
     class Meta:
-        ordering = ['-add_date']
+        ordering = ('-add_date',)
+        verbose_name = 'Компьютер'
+        verbose_name_plural = 'Компьютеры'
 
 
 class Host(models.Model):
@@ -174,36 +219,60 @@ class Host(models.Model):
                                  verbose_name='Виртуальные машины',
                                  through='HostVirtualMachine')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Хост'
+        verbose_name_plural = 'Хосты'
+
 
 class VirtualMachine(models.Model):
     """Виртуальные машины."""
     name = models.CharField('Имя виртуальной машины', max_length=100)
     uptime = models.DurationField(default=timedelta(seconds=1))
-    vm_id = models.CharField('ID виртуальной машины',
-                             max_length=100,
-                             default='Не определен')
+    vm_id = models.CharField(
+        'ID виртуальной машины',
+        max_length=100,
+        default='Не определен'
+    )
+
+    def __str__(self):
+        return self.name
 
     class Meta:
-        ordering = ['name']
+        ordering = ('name',)
+        verbose_name = 'Виртуальная машина'
+        verbose_name_plural = 'Виртуальные машины'
 
 
 class HostVirtualMachine(models.Model):
     "Связь виртуальных машин с хостами."
-    host = models.ForeignKey('Host',
-                             on_delete=models.CASCADE,
-                             related_name='host_vm')
-    vm = models.ForeignKey('VirtualMachine',
-                           on_delete=models.CASCADE,
-                           related_name='host_vm')
+    host = models.ForeignKey(
+        'Host',
+        on_delete=models.CASCADE,
+        related_name='host_vm',
+        verbose_name='Хост'
+    )
+    vm = models.ForeignKey(
+        'VirtualMachine',
+        on_delete=models.CASCADE,
+        related_name='host_vm',
+        verbose_name='Виртуальная машина'
+    )
 
 
 class VMAdapter(models.Model):
     """Сетевые адаптеры виртуальных машин."""
     mac = models.CharField('Мак-адрес', max_length=50)
     vlan = models.IntegerField('VlanID')
-    vm = models.ForeignKey(VirtualMachine,
-                           related_name='adapter',
-                           on_delete=models.CASCADE)
+    vm = models.ForeignKey(
+        VirtualMachine,
+        related_name='adapter',
+        on_delete=models.CASCADE,
+        verbose_name='Виртуальная машина'
+    )
 
     def is_duplicate(self):
         """Метод для определения дубликатов мак-адресов."""
--- invent_pc/users/admin.py
+++ invent_pc/users/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from comps.admin import NoAddAdmin
+from .models import ADUsers, Radius, VPN
+
+
+@admin.register(ADUsers)
+class ADuserAdmin(NoAddAdmin):
+    fields = (
+        'fio',
+        'login',
+        'email',
+        'status',
+        'rdlogin',
+        'vpn'
+    )
+    list_display_links = ('id', 'fio')
+    search_fields = (
+        'fio',
+        'login',
+        'email',
+    )
+    list_filter = ('status',)
+
+
+@admin.register(Radius)
+class RadiusAdmin(NoAddAdmin):
+    fields = (
+        'fio',
+        'login',
+        'status',
+    )
+    list_display_links = ('id', 'fio')
+    search_fields = (
+        'fio',
+        'login',
+    )
+    list_filter = ('status',)
+
+
+@admin.register(VPN)
+class VPNAdmin(NoAddAdmin):
+    fields = (
+        'login',
+        'comment',
+        'status',
+    )
+    list_display_links = ('id', 'login')
+    search_fields = (
+        'login',
+        'comment',
+    )
+    list_filter = ('status',)
